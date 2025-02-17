6ea39b856d74c1c454ac233d974f98dcc9db5900
Duc854 <nmduc854@gmail.com>
2025-02-17 14:35:10 +0700

    ci(structure): change interface folder v2

---
+++ api/Dtos/Problem/ProblemDetailDto.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Model;
+namespace api.Dtos.Problem
+{
+    public class ProblemDetailDto
+    {
+        public string ProblemId { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Detail { get; set; } = string.Empty;
+        public string Input { get; set; } = string.Empty;
+        public string Output { get; set; } = string.Empty;
+        [Range(0, 100)]
+        public int TotalPoint { get; set; } = 0;
+        public int TimeLimit { get; set; }
+        public int MemoryLimit { get; set; }
+        public string? Author { get; set; }
+        public string? Solution { get; set; }
+    }
+}
