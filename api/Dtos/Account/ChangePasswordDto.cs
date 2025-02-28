f676f4d6e4443363a4c3501ab75465eaa60cc926
Galaxy-eyes2701 <khanhdong27012003@gmail.com>
2025-02-28 09:27:20 +0700

    init

---
+++ api/Dtos/Account/ChangePasswordDto.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Dtos.Account
+{
+    public class ChangePasswordDto
+    {
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? OldPassword { get; set; }
+        public string? NewPassword { get; set; }
+        public string? ConfirmPassword { get; set; }
+        public string? OtpCode { get; set; }
+    }
+}
