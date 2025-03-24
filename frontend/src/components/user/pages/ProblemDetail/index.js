34b2aa43305092ab29a2fb4aded05e6ade4f7bdf
ahntuann <ahntuan284@gmail.com>
2025-03-24 10:46:44 +0700

    Fix conflict merge

--- frontend/src/components/user/pages/ProblemDetail/index.js
+++ frontend/src/components/user/pages/ProblemDetail/index.js
@@ -82,12 +82,7 @@ const ProblemDetail = () => {
                 </p>
             </div>
             <div className={cs('actions')}>
-                <Link
-                    to={`/practice?id=${problemId}`}
-                    className={cs('button', 'primary')}
-                    target="blank"
-                    rel="noopener"
-                >
+                <Link to={`/practice?id=${problemId}`} className={cs('button', 'primary')}>
                     Làm bài
                 </Link>
                 <Link
